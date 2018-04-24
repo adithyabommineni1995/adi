58319059e1db5816308857aade2e5246fd5b0db2
adithya <ad749464@dal.ca>
2018-04-24 13:39:57 -0700

    second commit

--- task1.py
+++ task1.py
@@ -1,1 +1,14 @@
-http
+import functools
+def nested_sum():
+        t = 0
+        list_n=[0,[1,2],3,[4,5]]
+        for j in list_n:
+                if(isinstance(j,list)):
+                        t = t + functools.reduce((lambda x,y: x+y) ,j)
+                else:
+                        t=t+j
+        print(t)
+nested_sum()
+
+
+
